a63b7ceacbb7fd3c991381328252ccca2ae3b325
Heimdall <heimdallthor02@gmail.com>
2019-11-25 07:38:54 +1100

    2-issue update the p2p folder name and also read ENV variable to determinate testnet

--- tss.go
+++ tss.go
@@ -9,6 +9,7 @@ import (
 	"fmt"
 	"math/big"
 	"net/http"
+	"os"
 	"strconv"
 	"sync"
 	"time"
@@ -153,7 +154,9 @@ func (t *Tss) keygen(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	types.Network = types.TestNetwork
+	if os.Getenv("NET") == "testnet" {
+		types.Network = types.TestNetwork
+	}
 	resp := KeyGenResp{
 		PubKey:     newPubKey,
 		BNBAddress: addr.String(),
