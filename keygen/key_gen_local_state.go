1e2e47dbeeb06f95c61550bd6f19d46ce3d69e1a
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-01-28 10:12:29 -0500

    use lowercase b

--- keygen/key_gen_local_state.go
+++ keygen/key_gen_local_state.go
@@ -23,7 +23,7 @@ type KeyGenResp struct {
 	PubKey      string        `json:"pub_key"`
 	PoolAddress string        `json:"pool_address"`
 	Status      common.Status `json:"status"`
-	Blame       common.Blame  `json:"Blame"`
+	Blame       common.Blame  `json:"blame"`
 }
 
 func SaveLocalStateToFile(filePathName string, state common.KeygenLocalStateItem) error {
