c81ddd8abeecfb0c505ac0cbe4613e5e9150a6f9
Son of Odin <sonofodin-thorchain@protonmail.com>
2020-02-04 14:50:44 -0500

    linting fix

--- cmd/tss/main.go
+++ cmd/tss/main.go
@@ -71,7 +71,7 @@ func main() {
 
 // parseFlags - Parses the cli flags
 func parseFlags() (generalConf common.GeneralConfig, tssConf common.TssConfig, p2pConf p2p.P2PConfig) {
-	//we setup the configure for the general configuration
+	// we setup the configure for the general configuration
 	flag.StringVar(&generalConf.TssAddr, "tss-port", "127.0.0.1:8080", "tss port")
 	flag.StringVar(&generalConf.InfoAddr, "info-port", ":8081", "info port")
 	flag.BoolVar(&generalConf.Help, "h", false, "Display Help")
