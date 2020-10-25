560cb1d0d6600a9b999b7c7d4ca9a15fa3ef8a9a
Ford Betelgeuse <ford.betelgeusedent@gmail.com>
2020-10-26 10:45:00 +1100

    fix bug of wrong freq calculation

--- common/tss_helper.go
+++ common/tss_helper.go
@@ -11,8 +11,6 @@ import (
 	"io"
 	"math/big"
 	"os"
-	"sort"
-	"strconv"
 	"strings"
 
 	"github.com/binance-chain/tss-lib/ecdsa/keygen"
@@ -103,29 +101,18 @@ func getHighestFreq(confirmedList map[string]string) (string, int, error) {
 		return "", 0, errors.New("empty input")
 	}
 	freq := make(map[string]int, len(confirmedList))
-	hashPeerMap := make(map[string]string, len(confirmedList))
-	for peer, n := range confirmedList {
+	for _, n := range confirmedList {
 		freq[n]++
-		hashPeerMap[n] = peer
 	}
-
-	sFreq := make([][2]string, 0, len(freq))
-	for n, f := range freq {
-		sFreq = append(sFreq, [2]string{n, strconv.FormatInt(int64(f), 10)})
-	}
-	sort.Slice(sFreq, func(i, j int) bool {
-		if sFreq[i][1] > sFreq[j][1] {
-			return true
-		} else {
-			return false
+	maxFreq := -1
+	var data string
+	for key, counter := range freq {
+		if counter > maxFreq {
+			maxFreq = counter
+			data = key
 		}
-	},
-	)
-	freqInt, err := strconv.Atoi(sFreq[0][1])
-	if err != nil {
-		return "", 0, err
 	}
-	return sFreq[0][0], freqInt, nil
+	return data, maxFreq, nil
 }
 
 func GetMsgRound(wireMsg *messages.WireMessage, partyID *btss.PartyID) (blame.RoundInfo, error) {
