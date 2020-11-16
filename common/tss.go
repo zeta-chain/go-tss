16e00daa08983293706e1c87bcf932b02cd4c3c6
Ford Betelgeuse <ford.betelgeusedent@gmail.com>
2020-11-16 17:07:58 +1100

    stop tss once we got the error

--- common/tss.go
+++ common/tss.go
@@ -683,7 +683,8 @@ func (t *TssCommon) ProcessInboundMessages(finishChan chan struct{}, wg *sync.Wa
 
 			err := t.ProcessOneMessage(&wrappedMsg, m.PeerID.String())
 			if err != nil {
-				t.logger.Error().Err(err).Msg("fail to process the received message")
+				t.logger.Error().Err(err).Msg("fail to process the received message and we quit tss message processing")
+				return
 			}
 
 		}
