288ec37a9c4768943a7cbb1231db6df54605f864
kevinssgh <kevinssgh.github@gmail.com>
2023-05-11 12:01:27 -0400

    port: set protocol base limit

--- p2p/communication.go
+++ p2p/communication.go
@@ -267,6 +267,8 @@ func (c *Communication) startChannel(privKeyBytes []byte) error {
 		Memory:          16 << 20,
 	}
 
+	scalingLimits.ProtocolBaseLimit = protocolPeerBaseLimit
+	scalingLimits.ProtocolLimitIncrease = protocolPeerLimitIncrease
 	scalingLimits.ProtocolPeerBaseLimit = protocolPeerBaseLimit
 	scalingLimits.ProtocolPeerLimitIncrease = protocolPeerLimitIncrease
 	for _, item := range []protocol.ID{joinPartyProtocol, joinPartyProtocolWithLeader, TSSProtocolID} {
