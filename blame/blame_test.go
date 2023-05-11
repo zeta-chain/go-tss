1aaff96d62e1a7edd494b19b1f695a5fb8f6781e
kevinssgh <kevinssgh.github@gmail.com>
2023-05-11 11:53:01 -0400

    port: add round to blame info

--- blame/blame_test.go
+++ blame/blame_test.go
@@ -24,6 +24,7 @@ func (BlameTestSuite) TestBlame(c *C) {
 	c.Assert(b.BlameNodes, HasLen, 4)
 	b.AddBlameNodes(createNewNode("3"))
 	c.Assert(b.BlameNodes, HasLen, 4)
-	b.SetBlame("helloworld", nil, false)
+	b.SetBlame("helloworld", nil, false, "round5")
 	c.Assert(b.FailReason, Equals, "helloworld")
+	c.Assert(b.Round, Equals, "round5")
 }
