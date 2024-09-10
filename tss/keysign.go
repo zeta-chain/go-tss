05876ac6d66af8cb87162a4d89335017fc2cadf3
Alex Gartner <alexg@zetachain.com>
2024-09-10 14:19:49 -0700

    Cherry pick changes from #1 and #7 (#27)

--- tss/keysign.go
+++ tss/keysign.go
@@ -219,6 +219,8 @@ func (t *TssServer) KeySign(req keysign.Request) (keysign.Response, error) {
 		t.p2pCommunication.ReleaseStream(msgID)
 		t.signatureNotifier.ReleaseStream(msgID)
 		t.partyCoordinator.ReleaseStream(msgID)
+
+		t.partyCoordinator.RemovePeerGroup(msgID)
 	}()
 
 	localStateItem, err := t.stateManager.GetLocalState(req.PoolPubKey)
