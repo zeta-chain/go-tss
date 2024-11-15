ab13fd6681dd1e7fea3620a9bf59955978287e0f
Alex Gartner <git@agartner.com>
2024-11-14 22:47:02 -0800

    fix infinite discovery address leak

--- p2p/discovery_test.go
+++ p2p/discovery_test.go
@@ -83,4 +83,9 @@ func TestDiscovery(t *testing.T) {
 	assert.Equal(t, 4, len(comm2.host.Peerstore().Peers()))
 	assert.Equal(t, 4, len(comm3.host.Peerstore().Peers()))
 	assert.Equal(t, 4, len(comm4.host.Peerstore().Peers()))
+
+	assert.Equal(t, 3, len(comm.discovery.knownPeers))
+	for peer, knownPeers := range comm.discovery.knownPeers {
+		assert.LessOrEqual(t, len(knownPeers.Addrs), 4, "%s has more than 4 addresses (%d)?", peer.String(), len(knownPeers.Addrs))
+	}
 }
